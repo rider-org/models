ce0527f2299d47a00b78dc31483667bcb0ec10c8
Tahmid Ahmed <tahmid@tahmid.io>
2025-02-24 00:20:31 -0500

    Add default ApiResponse types

---
+++ types/api.ts
@@ -0,0 +1,28 @@
+type Prettify<T> = {
+  [K in keyof T]: T[K];
+} & {};
+
+export type SuccessType<T = unknown, TDebug = never> = {
+  success: true;
+  message: string;
+  data: T;
+  _debug: TDebug;
+};
+
+export type ErrorType<TDebug = never> = {
+  success: false;
+  message: string;
+  _debug: TDebug;
+};
+
+/**
+ * Default API standard response.
+ * If you are in development, simply pass a debug object type to pass in additional information, without leaking it into production by accident.
+ */
+export type ApiDefault<T = unknown, TDebug = never> = Prettify<
+  SuccessType<T, TDebug> | ErrorType<TDebug>
+>;
+
+export type DebugDefault<T = any> = Prettify<
+  { message: string; date: Date } & Record<string, T>
+>;
